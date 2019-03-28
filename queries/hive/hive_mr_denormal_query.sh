8e057810e72c2b0b87e53a2ce341cf9cb481bc5e
Fisser <mfisser@sva.de>
2019-03-28 10:21:40 +0100

    initial

--- queries/hive/hive_mr_denormal_query.sh
+++ queries/hive/hive_mr_denormal_query.sh
@@ -1,92 +1,155 @@
 #!/bin/bash
-echo "-------------------------------------- HIVE QUERY DENORMALIZED ---------------------------------"
-for i in 1 2
+
+if [ ! -d $1 ] && [ ! -d $2 ]; then
+mkdir -p results
+cd results
+resultPath=hive_mr_denormal_query_sf"$1"_result.txt
+rm -f $resultPath
+touch $resultPath
+echo "-------------------------------------- CREATED PATH IF NOT EXIST ---------------------------------" |& tee -a $resultPath
+echo "-------------------------------------- "$resultPath" ---------------------------------"|& tee -a $resultPath
+
+echo "-------------------------------------- HIVE QUERY DENORMALIZED ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
+echo ""|& tee -a $resultPath
+
+for i in $(seq 1 $2)
 do
 echo "--------------------------------------STARTED QUERY 1."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice*(1-l_discount)) as sum_disc_price, sum(l_extendedprice*(1-l_discount)*(1+l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order from denormalized where l_shipdate <= date '1998-12-01' - interval '90' day group by l_returnflag, l_linestatus order by l_returnflag, l_linestatus;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 1."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 2."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; "&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 2."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 3."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select o_orderkey, sum(l_extendedprice*(1-l_discount)) as revenue, o_orderdate, o_shippriority from denormalizedwhere c_mktsegment = 'BUILDING' and o_orderdate < date '1995-03-15' and l_shipdate > date '1995-03-15' group by o_orderkey, o_orderdate, o_shippriority order by revenue desc, o_orderdate limit 10;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 3."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 4"$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select o_orderpriority, count (distinct o_orderkey) as order_count from denormalizedwhere o_orderdate >= date '1993-07-01' and o_orderdate < date '1993-07-01' + interval '3' month and l_commitdate < l_receiptdate group by o_orderpriority order by o_orderpriority;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 4."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 5."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select n_name, sum(l_extendedprice * (1 - l_discount)) as revenue from denormalized where r_name = 'ASIA' and o_orderdate >= date '1994-01-01' and o_orderdate < date '1994-01-01' + interval '1' year group by n_name order by revenue desc;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 5."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 6."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select sum(l_extendedprice*l_discount) as revenue from denormalized where l_shipdate >= date '1994-01-01' and l_shipdate < date '1994-01-01' + interval '1' year and l_discount between 0.06 - 0.01 and 0.06 + 0.01 and l_quantity < 24;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 6."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 7."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select  n2_name, n_name,  year(l_shipdate) as l_year, sum(l_extendedprice * (1 - l_discount)) as revenue from denormalized where ((n2_name = 'FRANCE' and n_name = 'GERMANY') or (n2_name = 'GERMANY' and n_name = 'FRANCE')) and l_shipdate between date '1995-01-01' and date '1996-12-31' group by n2_name, n_name, year(l_shipdate) order by n2_name, n_name, l_year;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 7."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 8."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select o_year, sum(case when nation = 'BRAZIL' then volume else 0 end) / sum(volume) as mkt_share from ( select year(o_orderdate) as o_year, l_extendedprice * (1-l_discount) as volume, n2_name as nation from denormalized where r_name = 'AMERICA' and o_orderdate between date '1995-01-01' and date '1996-12-31' and p_type = 'ECONOMY ANODIZED STEEL') as all_nations group by o_year order by  o_year;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 8."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 9."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select nation, o_year, sum(amount) as sum_profit from (select n2_name as nation, extract(year from o_orderdate) as o_year, l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount from denormalized where p_name like '%green%' ) as profit group by nation, o_year order by nation, o_year desc;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 9."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 10."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select c_custkey, c_name, sum(l_extendedprice * (1 - l_discount)) as revenue, c_acctbal, n_name, c_address, c_phone, c_comment from denormalized where o_orderdate >= date '1993-10-01' and o_orderdate < date '1993-10-01' + interval '3' month and l_returnflag = 'R' group by c_custkey, c_name, c_acctbal, c_phone, n_name, c_address, c_comment order by revenue desc limit 20;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 10."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 11."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; "&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 11."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 12."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select l_shipmode, sum(case when o_orderpriority ='1-URGENT' or o_orderpriority ='2-HIGH' then 1 else 0 end) as high_line_count, sum(case when o_orderpriority <> '1-URGENT' and o_orderpriority <> '2-HIGH' then 1 else 0 end) as low_line_count from denormalized where l_shipmode in ('MAIL', 'SHIP') and l_commitdate < l_receiptdate and l_shipdate < l_commitdate and l_receiptdate >= date '1994-01-01' and l_receiptdate < date '1994-01-01' + interval '1' year group by l_shipmode order by l_shipmode;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 12."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 13."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; "&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 13."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 14."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select 100.00 * sum(case when p_type like 'PROMO%' then l_extendedprice*(1-l_discount) else 0 end) / sum(l_extendedprice * (1 - l_discount)) as promo_revenue from denormalized where l_partkey = p_partkey and l_shipdate >= date '1995-09-01' and l_shipdate < date '1995-09-01' + interval '1' month;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 14."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 15."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; set hive.auto.convert.join=false; create view v_revenue_denorm (supplier_no, total_revenue) as select l_suppkey,sum(l_extendedprice * (1 - l_discount)) from denormalized where l_shipdate >= date '1996-01-01' and l_shipdate < date '1996-01-01' + interval '3' month group by l_suppkey; select s_suppkey,s_name,s_address,s_phone,max(total_revenue) as total_revenue from denormalized, v_revenue_denorm where s_suppkey = supplier_no group by s_suppkey,s_name,s_address,s_phone order by total_revenue desc limit 1; set hive.auto.convert.join=true;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 15."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 16."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select p_brand, p_type, p_size, count(distinct s_suppkey) as supplier_cnt from denormalized where p_brand <> 'Brand#45' and p_type not like 'MEDIUM POLISHED%' and p_size in (49, 14, 23, 45, 19, 3, 36, 9) and s_comment not like '%Customer%Complaints%' group by p_brand, p_type, p_size order by  supplier_cnt desc,  p_brand,  p_type, p_size;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 16."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 17."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select sum(l_extendedprice) / 7.0 as avg_yearly from denormalized a where p_brand = 'Brand#23' and p_container = 'MED BOX' and l_quantity < ( select 0.2 * avg(l_quantity) from denormalized b where a.l_partkey = b.p_partkey );"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 17."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 18."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select c_name, c_custkey, o_orderkey, o_orderdate, o_totalprice, sum(l_quantity) from denormalized group by c_name, c_custkey, o_orderkey, o_orderdate, o_totalprice having sum(l_quantity) > 300 order by o_totalprice desc, o_orderdate;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 18."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 19."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select sum(l_extendedprice * (1 - l_discount) ) as revenue from denormalized where ( p_brand = 'Brand#12'and p_container in ( 'SM CASE', 'SM BOX', 'SM PACK', 'SM PKG') and l_quantity >= 1 and l_quantity <= 1 + 10 and p_size between 1 and 5 and l_shipmode in ('AIR', 'AIR REG') and l_shipinstruct = 'DELIVER IN PERSON') or (p_brand = 'Brand#23' and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK') and l_quantity >= 10 and l_quantity <= 10 + 10 and p_size between 1 and 10 and l_shipmode in ('AIR', 'AIR REG') and l_shipinstruct = 'DELIVER IN PERSON' ) or (p_brand = 'Brand#34' and p_container in ( 'LG CASE', 'LG BOX', 'LG PACK', 'LG PKG') and l_quantity >= 20 and l_quantity <= 20 + 10 and p_size between 1 and 15 and l_shipmode in ('AIR', 'AIR REG') and l_shipinstruct = 'DELIVER IN PERSON');"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 19."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 20."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; "&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 20."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 21."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; select s_name, count(*) as numwait from denormalized l1 where o_orderstatus = 'F' and l1.l_receiptdate > l1.l_commitdate and exists ( select * from denormalized l2 where l2.l_orderkey = l1.l_orderkey and l2.l_suppkey <> l1.l_suppkey ) and not exists ( select * from denormalized l3 where l3.l_orderkey = l1.l_orderkey and l3.l_suppkey <> l1.l_suppkey and l3.l_receiptdate > l3.l_commitdate ) and s_nationkey = n2_nationkey and n2_name = 'SAUDI ARABIA' group by s_name order by numwait desc, s_name limit 100;"&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 21."$i"--------------------------------"
 
 echo "--------------------------------------STARTED QUERY 22."$i" ---------------------------------"
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "set hive.execution.engine=mr; "&>>/opt/data/sf1/hive_mr_normal_query_result.txt
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 22."$i"--------------------------------"
+echo ""|& tee -a $resultPath
 done
+else
+echo "Parameters have to be defined for this script. Paramater 1 for scale factor. Paramter 2 for number of loops."
+echo "Example ./imapalatest.sh 1 2"
+exit 2
+fi
