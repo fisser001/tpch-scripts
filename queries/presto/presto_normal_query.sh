54b3fdee41318ac4dc4a2dd2e56b0f6bd62411b6
Fisser <mfisser@sva.de>
2019-03-30 15:25:20 +0100

    initial

--- queries/presto/presto_normal_query.sh
+++ queries/presto/presto_normal_query.sh
@@ -9,99 +9,165 @@ touch $resultPath
 echo "-------------------------------------- CREATED PATH IF NOT EXIST ---------------------------------"|& tee -a $resultPath|& tee -a $resultPath
 echo "-------------------------------------- "$resultPath" ---------------------------------"|& tee -a $resultPath|& tee -a $resultPath
 
-echo "-------------------------------------- PRESTO QUERY NORMALIZED ---------------------------------"|& tee -a $resultPath|& tee -a $resultPath
+echo "-------------------------------------- PRESTO QUERY NORMAL ---------------------------------"|& tee -a $resultPath|& tee -a $resultPath
 echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo ""|& tee -a $resultPath
 
 for i in $(seq 1 $2)
 do
 echo "--------------------------------------STARTED QUERY 1."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 1."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 2."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 2."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 3."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 3."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
-echo "--------------------------------------STARTED QUERY 4"$i" ---------------------------------"|& tee -a $resultPath
+echo "--------------------------------------STARTED QUERY 4."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 4."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 5."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 5."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 6."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 6."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 7."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 7."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 8."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 8."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 9."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 9."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 10."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 10."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 11."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 11."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 12."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 12."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 13."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 13."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 14."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 14."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 15."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 15."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 16."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 16."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 17."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 17."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 18."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 18."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 19."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 19."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 20."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 20."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 21."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 21."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 
 echo "--------------------------------------STARTED QUERY 22."$i" ---------------------------------"|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e ""|& tee -a $resultPath
+echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 22."$i"--------------------------------"|& tee -a $resultPath
+echo ""|& tee -a $resultPath
 done
 else
 echo "Parameters have to be defined for this script. Paramater 1 for scale factor. Paramter 2 for number of loops."
