787554a9aa5a8f9f3f60475d5079b41b9c777705
Fisser <mfisser@sva.de>
2019-03-30 17:55:02 +0100

    initial

--- queries/presto/presto_normal_query.sh
+++ queries/presto/presto_normal_query.sh
@@ -21,7 +21,7 @@ echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 echo "--------------------------------------Finished QUERY 1."$i"--------------------------------"|& tee -a $resultPath
 echo ""|& tee -a $resultPath
-d
+
 echo "--------------------------------------STARTED QUERY 2."$i" ---------------------------------"|& tee -a $resultPath
 echo $(date '+%d/%m/%Y %H:%M:%S.%3N')|& tee -a $resultPath
 /usr/local/bin/presto --server coordinator:8080 --catalog hive --schema default "select s_acctbal, s_name, n_name, p_partkey, p_mfgr, s_address, s_phone, s_comment from p_part, s_supplier, ps_partsupp, n_nation, r_region where p_partkey = ps_partkey and s_suppkey = ps_suppkey and p_size = 15 and p_type like '%BRASS' and s_nationkey = n_nationkey and n_regionkey = r_regionkey and r_name = 'EUROPE' and ps_supplycost = ( select min(ps_supplycost) from ps_partsupp, s_supplier, n_nation, r_region where p_partkey = ps_partkey and s_suppkey = ps_suppkey and s_nationkey = n_nationkey and n_regionkey = r_regionkey and r_name = 'EUROPE' ) order by s_acctbal desc, n_name, s_name, p_partkey limit 100;"|& tee -a $resultPath
