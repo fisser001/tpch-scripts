e233de01c5e05389ccca463e7d087b9fb1b4d522
Fisser <mfisser@sva.de>
2019-04-10 14:11:33 +0200

    initial

--- queries/hive/hive_normal_query.sh
+++ queries/hive/hive_normal_query.sh
@@ -4,7 +4,7 @@ query1 () {
 test1=""
 test1="!sh echo '--------------------------------------STARED QUERY $m.$i--------------------------------';"
 test1="$test1 !sh echo '$(date '+%d/%m/%Y %H:%M:%S.%3N')';"
-test1="$test1 set hive.execution.engine=$engine; set mapred.job.queue.name=$queuename; set tez.queue.name=$queuename;  SET mapreduce.framework.name=$framework;  select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price,  sum(l_extendedprice*(1-l_discount)) as sum_disc_price,  sum(l_extendedprice*(1-l_discount)*(1+l_tax)) as sum_charge,  avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price,  avg(l_discount) as avg_disc, count(*) as count_order  from l_lineitem  where l_shipdate <= date '1998-12-01' - interval '90' day  group by l_returnflag, l_linestatus  order by l_returnflag, l_linestatus;"
+#test1="$test1 set hive.execution.engine=$engine; set mapred.job.queue.name=$queuename; set tez.queue.name=$queuename;  SET mapreduce.framework.name=$framework;  select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price,  sum(l_extendedprice*(1-l_discount)) as sum_disc_price,  sum(l_extendedprice*(1-l_discount)*(1+l_tax)) as sum_charge,  avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price,  avg(l_discount) as avg_disc, count(*) as count_order  from l_lineitem  where l_shipdate <= date '1998-12-01' - interval '90' day  group by l_returnflag, l_linestatus  order by l_returnflag, l_linestatus;"
 test1="$test1 !sh echo '$(date '+%d/%m/%Y %H:%M:%S.%3N')';"
 test1="$test1 !sh echo '--------------------------------------Finished QUERY $m.$i--------------------------------';"
 }
