e790d89dbff6baba885bcc2d9d52f1bb4bc7bd62
Fisser <mfisser@sva.de>
2019-04-11 10:50:13 +0200

    initial

--- docker/docker_volumes.sh
+++ docker/docker_volumes.sh
@@ -2,17 +2,17 @@
 
 if [ ! -d $1 ]; then
 if [ "$1" = "create" ]; then
-docker volume create mydata
-docker volume create hadoop_datanode1
-docker volume create hadoop_datanode2
-docker volume create hadoop_datanode3
-docker volume create hadoop_namenode
-docker volume create hadoop_historyserver
-docker volume create pgdata
-docker volume create hive_metastore
-docker volume create hadoop_resourcemanager
-docker volume create hadoop_nodemanager1
-docker volume create hadoop_nodemanager2
+docker volume create mydata -d local-persist -o mountpoint=/data/mydata --name mydata
+docker volume create hadoop_datanode1 -d local-persist -o mountpoint=/data/hadoop_datanode1
+docker volume create hadoop_datanode2 -d local-persist -o mountpoint=/data/hadoop_datanode2
+docker volume create hadoop_datanode3 -d local-persist -o mountpoint=/data/hadoop_datanode3
+docker volume create hadoop_namenode -d local-persist -o mountpoint=/data/hadoop_namenode
+docker volume create hadoop_historyserver -d local-persist -o mountpoint=/data/hadoop_historyserver
+docker volume create pgdata -d local-persist -o mountpoint=/data/pgdata
+docker volume create hive_metastore -d local-persist -o mountpoint=/data/hive_metastore
+docker volume create hadoop_resourcemanager -d local-persist -o mountpoint=/data/rhadoop_resourcemanager
+docker volume create hadoop_nodemanager1 -d local-persist -o mountpoint=/data/hadoop_nodemanager1
+docker volume create hadoop_nodemanager2 -d local-persist -o mountpoint=/data/hadoop_nodemanager2
 fi
 
 if [ "$1" = "delete" ]; then
